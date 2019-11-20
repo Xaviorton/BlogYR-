81d57f411b4653ec3434d05fe68be00b1b13023d
ykshprsky <48822403+ykshprsky@users.noreply.github.com>
2019-11-20 10:10:56 +0200

    backend

---
+++ Blog/Controllers/MoreController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+using System.Data.Entity;
+using PagedList.Mvc;
+using PagedList;
+namespace Blog.Controllers
+{
+    public class MoreController : Controller
+    {
+       
+            ApplicationDbContext db = new ApplicationDbContext();
+            // GET: More
+            [HttpGet]
+            public ActionResult More(int? id)
+            {
+                var categories = db.Categories;
+                ViewBag.Categories = categories;
+                if (id == null)
+                {
+                    return HttpNotFound();
+                }
+                Product product = db.Products.Find(id);
+                if (product != null)
+                {
+                    return View(product);
+                }
+                return HttpNotFound();
+
+            }
+
+
+        }
+    }
+}
+}
