8919e68e568770319ea88420af59fc683374215f
Petro <pelexovsij@gmail.com>
2019-11-21 13:32:58 +0200

    Добавьте файлы проекта.

---
+++ Blog/Controllers/AddArController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using System.Data.Entity;
+using PagedList.Mvc;
+using PagedList;
+using Blog.Models;
+namespace Blog.Controllers
+{
+    public class AddArController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: AddAr
+        public ActionResult AddAr()
+        {
+          
+            var categories = context.Categories;
+            ViewBag.Categories = categories;
+            SelectList category = new SelectList(context.Categories, "CategoryId", "CategoryName");
+            ViewBag.Category = category;
+            //      ViewBag.ProductId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAr(Article article)
+        {
+
+            context.Articles.Add(article);
+            var category = context.Categories;
+
+
+            context.SaveChanges();
+            return RedirectToAction("../Home/");
+        }
+    }
+}
